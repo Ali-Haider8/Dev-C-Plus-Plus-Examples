755492a84abb2dd5f6f8559108670538ee249d6a
Ali H. <26713386+Ali-Haider8@users.noreply.github.com>
2026-07-23 13:24:12 +0300

    new file

---
+++ src/learn_struct/struct_lesson_point.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+struct Point
+{
+    int x;
+    int y;
+};
+
+int main()
+{
+    Point p1, p2;
+    p1.x = 10;
+    p1.y = 20;
+
+    p2.x = 30;
+    p2.y = 40;
+
+    cout << "sum of x: " << (p1.x + p2.x) << endl;
+    cout << "sum of y: " << (p1.y + p2.y) << endl;
+
+    return 0;
+}
