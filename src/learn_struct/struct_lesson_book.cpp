0289c9dce62df9f57c053754711f509b58113eb9
Ali H. <26713386+Ali-Haider8@users.noreply.github.com>
2026-07-23 13:06:53 +0300

    new file

---
+++ src/learn_struct/struct_lesson_book.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+using namespace std;
+
+struct Book
+{
+    string title;
+    string author;
+    int year;
+    float price;
+};
+
+int main()
+{
+
+    Book b1, b2;
+
+    b1.title = "Java";
+    b1.author = "Ali";
+    b1.year = 2017;
+    b1.price = 10.40;
+
+    b2.title = "Python";
+    b2.author = "Ali";
+    b2.year = 2026;
+    b2.price = 20.4;
+
+    cout << "title: " << b2.title << endl;
+    cout << "author: " << b2.author << endl;
+    cout << "year: " << b2.year << endl;
+    cout << "price: " << b2.price << endl;
+
+    return 0;
+}
