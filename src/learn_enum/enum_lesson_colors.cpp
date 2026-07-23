56154d4f430a609b8381f2d51b356fa21e067d21
Ali H. <26713386+Ali-Haider8@users.noreply.github.com>
2026-07-23 13:45:05 +0300

    new example

---
+++ src/learn_enum/enum_lesson_colors.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+enum Color { RED, GREEN, BLUE, YELLOW };
+
+int main() {
+    Color myColor = GREEN;
+
+    if (myColor == GREEN) {
+        cout << "The color is Green!" << endl;
+    }
+
+    cout << "GREEN Value = " << GREEN << endl;
+
+    return 0;
+}
